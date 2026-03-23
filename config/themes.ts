d6978879cb58bffafaf1de38e578a8bbe19425a3
Gabriel <60961939+gabrielfordevelopment@users.noreply.github.com>
2026-03-23 16:28:08 +0100

    themes
    Signed-off-by: Gabriel <60961939+gabrielfordevelopment@users.noreply.github.com>

--- config/themes.ts
+++ config/themes.ts
@@ -6,5 +6,9 @@ export interface ThemeOption {
 export const AVAILABLE_THEMES: Array<ThemeOption> = [
   { id: "light", name: "Light" },
   { id: "dark", name: "Dark" },
-  { id: "dracula", name: "Dracula" }
+  { id: "dracula", name: "Dracula" },
+  { id: "monokai", name: "Monokai" },
+  { id: "solarized-light", name: "Solarized Light" },
+  { id: "solarized-dark", name: "Solarized Dark" },
+  { id: "nord", name: "Nord" }
 ];
