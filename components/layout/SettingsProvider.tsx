3dfeda8fdbb6fb4dcb585cd7080925a1cf603487
Gabriel <60961939+gabrielfordevelopment@users.noreply.github.com>
2026-03-31 19:53:12 +0200

    fix flashing on loading
    Signed-off-by: Gabriel <60961939+gabrielfordevelopment@users.noreply.github.com>

--- components/layout/SettingsProvider.tsx
+++ components/layout/SettingsProvider.tsx
@@ -4,11 +4,18 @@ import { useEffect } from "react";
 import { useSettingsStore } from "@/store/useSettingsStore";
 
 export function SettingsProvider({ children }: { children: React.ReactNode }) {
+  const isLoaded = useSettingsStore((state) => state.isLoaded);
   const loadSettings = useSettingsStore((state) => state.loadSettings);
 
   useEffect(() => {
-    loadSettings();
-  }, [loadSettings]);
+    if (!isLoaded) {
+      loadSettings();
+    }
+  }, [isLoaded, loadSettings]);
+
+  if (!isLoaded) {
+    return null;
+  }
 
   return <>{children}</>;
 }
